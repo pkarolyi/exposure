7002b69aca3de2f9b914e3033557a83785421b6c
karolyi.peter <karolyi.peter@sch.bme.hu>
2016-10-06 18:13:07 +0200

    Css responsiveness fix, uploader set to store in file in development mode

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,18 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-      provider:              'AWS',
-      :aws_access_key_id     => ENV['S3_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET'],
-      :region                => ENV['S3_REGION']
-  }
-  config.fog_directory  = ENV['S3_BUCKET_NAME']
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
+if Rails.env.production? then
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+        provider:              'AWS',
+        :aws_access_key_id     => ENV['S3_KEY'],
+        :aws_secret_access_key => ENV['S3_SECRET'],
+        :region                => ENV['S3_REGION']
+    }
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage = :file
+  end
 end
